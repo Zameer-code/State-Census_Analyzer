35bc6434708693fbc2ba2c530652bff5f82d5c65
Zameer-code <84001846+Zameer-code@users.noreply.github.com>
2021-07-01 16:24:47 +0530

    State Census Analyser

---
+++ src/main/java/com/statecensusanalyzer/StateCensusAnalyser.java
@@ -0,0 +1,67 @@
+package com.statecensusanalyzer;
+
+/**
+ ************************************************************************************************************************************************************************* 
+ * Purpose: This Class is Implemented to Import and as well as Getting the State Census and Code 
+ * Through Csv file
+ * @author ZameerAhamad Ron <zameerraone96@gmail.com>
+ * @version 1.4
+ * @since 30-06-2021
+ *****************************************************************************************************************************************************************************
+ **/
+
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Paths;
+import java.util.Iterator;
+
+	    
+public class StateCensusAnalyser {
+	
+	public static <T>  int openCsvBuilder(String csvFilePath, Object myClass) throws CensusAnalyserException {
+        int counter = 0;
+        try {
+            Iterator<Object> myIterator = getIterator(csvFilePath, myClass);
+            while ( myIterator.hasNext() ) {
+                counter++;
+                Object myObj = myIterator.next();
+                System.out.println(myObj.toString());
+            }
+        } catch (CensusAnalyserException e){
+            throw e;
+        } catch (RuntimeException e){
+            throw new CensusAnalyserException(CensusAnalyserException.CensusExceptionType.DELIMITER_ISSUE,
+                    "might be some error related to delimiter at record no. : " +(counter+1));
+        }
+        return counter;
+    }
+
+    public static Iterator<Object> getIterator(String csvFilePath, Object myClass) throws CensusAnalyserException {
+        Reader reader = null;
+        CsvToBean<Object> csvToBean;
+        try {
+            reader = Files.newBufferedReader(Paths.get(csvFilePath));
+            csvToBean = new CsvToBeanBuilder(reader)
+                    .withType((Class) myClass)
+                    .withIgnoreLeadingWhiteSpace(true)
+                    .build();
+            Iterator<Object> myIterator = csvToBean.iterator();
+            return myIterator;
+        } catch (NoSuchFileException e) {
+            throw new CensusAnalyserException(CensusAnalyserException.CensusExceptionType.NO_SUCH_FILE,
+                    "no such file exists. Please enter correct file");
+        } catch (RuntimeException e){
+            throw new CensusAnalyserException(CensusAnalyserException.CensusExceptionType.INCORRECT_DATA_ISSUE,
+                    "delimiter error at line 1 OR might be some error " +
+                            "related to headers or incorrect extension / input file ");
+        } catch (IOException e) {
+            throw new CensusAnalyserException(CensusAnalyserException.CensusExceptionType.SOME_OTHER_IO_EXCEPTION,
+                    "Some other IO related exception");
+        }
+        
+    }
+}
---
+++ src/test/java/com/statecensusanalyzer/StateCensusAnalyserTest.java
@@ -0,0 +1,70 @@
+package com.statecensusanalyzer;
+
+import org.junit.Assert;
+import org.junit.Test;
+import java.io.IOException;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+import java.io.IOException;
+
+
+public class StateCensusAnalyserTest {
+		
+	 public static final String STATECENSUS_CSVFILE = "C:\\Users\\dell\\Desktop\\IndiaStateCensusData.csv";
+	    public static final String STATECODES_CSVFILE = "C:\\Users\\dell\\Desktop\\IndiaStateCensusCode.csv";
+	    public static final String WRONG_FILE = "/novalue.txt";
+	    @Test
+	    public void GivenTheStateCodesCsvFile_IfHasCorrectNumberOfRecords_ShouldReturnTrue() throws IOException {
+	        StateCensusAnalyser stateCensusAnalyser = new StateCensusAnalyser();
+	        try {
+	            int count = stateCensusAnalyser.openCsvBuilder(STATECODES_CSVFILE, StateCensus.class);
+	            System.out.println(count);
+	            Assert.assertEquals(29, count);
+	        } catch (CensusAnalyserException e) {
+	            e.printStackTrace();
+	        }
+	    }
+	    @Test
+	    public void GivenTheStateCodeCsvFile_IfDoesntExist_ShouldThrowCensusAnalyserException() throws IOException {
+	        try {
+	            int count = StateCensusAnalyser.openCsvBuilder(WRONG_FILE, StateCensus.class);
+	        } catch (CensusAnalyserException e) {
+	            e.printStackTrace();
+	            Assert.assertEquals(CensusAnalyserException.CensusExceptionType.NO_SUCH_FILE, e.type);
+	        }
+	    }
+
+	    @Test
+	    public void GivenTheStateCodeCsvFile_WhenCorrect_ButFileExtensionIncorrect_ShouldThrowCensusAnalyserException()
+	            throws IOException {
+	        try {
+	            int count = StateCensusAnalyser.openCsvBuilder(STATECODES_CSVFILE, StateCensus.class);
+	        } catch (CensusAnalyserException e) {
+	            e.printStackTrace();
+	            Assert.assertEquals(CensusAnalyserException.CensusExceptionType.INCORRECT_DATA_ISSUE, e.type);
+	        }
+	    }
+
+	    @Test
+	    public void GivenTheStateCodeCSVFile_WhenCorrect_ButDelimiterIncorrect_ReturnsCensusAnalyserException()
+	            throws IOException {
+	        try {
+	            int count = StateCensusAnalyser.openCsvBuilder(STATECODES_CSVFILE, StateCensus.class);
+	        } catch (CensusAnalyserException e) {
+	            e.printStackTrace();
+	            Assert.assertEquals(CensusAnalyserException.CensusExceptionType.DELIMITER_ISSUE, e.type);
+
+	        }
+	    }
+
+	    @Test
+	    public void whenCorrectCodeCSVFile_ButHeaderIncorrect_ShouldReturnFalse() throws IOException {
+	        try {
+	            int count = StateCensusAnalyser.openCsvBuilder(STATECODES_CSVFILE, StateCensus.class);
+	        } catch (CensusAnalyserException e) {
+	            e.printStackTrace();
+	            Assert.assertEquals(CensusAnalyserException.CensusExceptionType.INCORRECT_DATA_ISSUE, e.type);
+	        }
+	    }
+
+}
